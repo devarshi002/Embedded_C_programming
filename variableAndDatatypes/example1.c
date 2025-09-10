38126ba9efdc5accc5c3b6aa19db0271bb770797
Devarshi Tambulkar <devarshitambulkar2@gmail.com>
2025-09-10 23:12:41 +0530

    first commit

---
+++ variableAndDatatypes/example1.c
@@ -0,0 +1,11 @@
+# include<stdio.h>
+
+int main() {
+    int a = 10; // integer variable
+    float b = 5.5; // floating point variable
+    char c = 'A'; // character variable
+    printf("Integer: %d\n", a);
+    printf("Float: %f\n", b);
+    printf("Character: %c\n", c);
+    return 0;
+}
